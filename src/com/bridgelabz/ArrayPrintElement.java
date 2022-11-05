f5948ff30821de973fa00d9b087e4706abac292a
kolhechetna <kolhe.chetna@gmail.com>
2022-11-05 08:17:59 +0530

    Added:Print the elements of an array

---
+++ src/com/bridgelabz/ArrayPrintElement.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class ArrayPrintElement {
+	public static void main(String[] args) {
+
+		int[] a = { 2, 3, 4, 5, 6, 7, 8, 9 };
+
+		for (int i = 0; i <= a.length - 1; i++) {
+			System.out.println(a[i] + " ");
+		}
+	}
+
+}
